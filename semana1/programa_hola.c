0ab257fa7dc6e56ad6a5c290652175b118591641
Cuevas Cazares Jose Pablo <jpspora@gmail.com>
2018-08-08 09:55:33 -0500

    /* Jose hizo el primer programa hoy*/
    \

---
+++ semana1/programa_hola.c
@@ -0,0 +1,11 @@
+/*Este es un comentario hecho por mi, en el edificio de la DCI
+*/
+/*Creado el ocho de agosto del dos mil dieciocho*/
+#include<stdio.h>
+int main()
+{
+printf("Hola Jose \n");
+printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
+
