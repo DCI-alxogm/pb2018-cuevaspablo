e545c890e05fdf1da44feed0ba5c4124c2c90190
Cuevas Cazares Jose Pablo <jpspora@gmail.com>
2018-08-16 14:08:12 -0500

    Hoy fue realizado un programa de enteros por mi

---
+++ semana2/enteros.c
@@ -0,0 +1,15 @@
+/*Este programa realizara calculos de numeros enteros*/
+#include<stdio.h>
+int main()
+{
+int e,a,b,c,d;
+printf("Dame cuatro numeros enteros \n");
+scanf("%i",&a);
+scanf("%i",&b);
+scanf("%i",&c);
+scanf("%i",&d);
+(e=(a+b)*(c/d));
+
+printf("\n El numero es: %i \n", e);
+return 0;
+}
