eb21570f731073c5325f4bca2910253dd5a247fc
Cuevas Cazares Jose Pablo <jpspora@gmail.com>
2018-08-16 14:18:37 -0500

    Hoy realice un programa de numeros enteros

--- semana2/enteros.c
+++ semana2/enteros.c
@@ -2,14 +2,34 @@
 #include<stdio.h>
 int main()
 {
-int e,a,b,c,d;
+int e,q,w,p,a,b,c,d;
 printf("Dame cuatro numeros enteros \n");
 scanf("%i",&a);
 scanf("%i",&b);
 scanf("%i",&c);
 scanf("%i",&d);
 (e=(a+b)*(c/d));
-
 printf("\n El numero es: %i \n", e);
+printf("Dame cuatro numeros enteros \n");
+scanf("%i",&a);
+scanf("%i",&b);
+scanf("%i",&c);
+scanf("%i",&d);
+(p=((a+b)*c)/d);
+printf("\n El numero es: %i \n", p);
+printf("Dame cuatro numeros enteros \n");
+scanf("%i",&a);
+scanf("%i",&b);
+scanf("%i",&c);
+scanf("%i",&d);
+(q=(a+b)*c/d);
+printf("\n El numero es: %i \n", q);
+printf("Dame cuatro numeros enteros \n");
+scanf("%i",&a);
+scanf("%i",&b);
+scanf("%i",&c);
+scanf("%i",&d);
+(w=a+(b*c)/d);
+printf("\n El numero es: %i \n", w);
 return 0;
 }
