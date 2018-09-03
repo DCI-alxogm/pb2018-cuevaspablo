aa17b6e72e152106efa6a9eb150bd491fb97c710
Cuevas Cazares Jose Pablo <jpspora@gmail.com>
2018-09-03 07:51:25 -0500

    Realicé cambios hoy porque había subido un archivo erróneo

--- semana4/ejemplowhile2.c
+++ semana4/ejemplowhile2.c
@@ -1,4 +1,4 @@
-/*Este es el segundo programa que utiliza el comando while*/
+/*Este es el segundo programa ejemplo que utiliza el comando while*/
 #include<stdio.h>
 int main()
 {
@@ -8,4 +8,15 @@ int n=10;
 int op=1;
 delta=(final-inicial)/n;
 while (op==1){
-temp
+TempK=0.;
+TempC=inicial;
+while (TempC<=final){
+TempK=TempC+273.15;
+printf("%f %f\n",TempC,TempK);
+TempC=TempC+delta;   //TempC=delta;
+}
+printf("¿Quiere realizar otra operación?. Presiona 1 para sí, presiona 2 para no\n");
+scanf("%i",&op);
+}
+return 0;
+}
