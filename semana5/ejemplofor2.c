cf50d7b187e5705e8ce10aa2557ae3b6e85dceb3
Cuevas Cazares Jose Pablo <jpspora@gmail.com>
2018-09-10 08:00:21 -0500

    Modifiqué un programa porquetenía un peueño error en él

--- semana5/ejemplofor2.c
+++ semana5/ejemplofor2.c
@@ -4,16 +4,26 @@
 int main()
 {
 float e,p;
-int n,j;
+int n,j,k=1,l;
+while (k==1){
 printf("Defina el número de pasos\n");
 scanf("%i",&n);
-printf("Defina otra variable\n");
-scanf("%i",&p);
 for (j=0;j<n;j++) 
 {
 e=exp(j);
 printf("%i\t",j);
 printf("%f\n",e);
+}
+printf("¿Quiere realizar otra operación?, pulse 1 para sí y 0 para no\n");
+scanf("%i",&l);
+if (l!=1) {
+printf("Se acabó el programa.\n");
+k=0;
+}
+else if (l=1) {
+k=1;
+printf("Se reinicia el programa\n");
+}
 }
 return 0;
 }
