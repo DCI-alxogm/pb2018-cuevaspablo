6d39386a58e8606b43296e28eedfbcfbb86b555c
Cuevas Cazares Jose Pablo <jpspora@gmail.com>
2018-09-12 08:35:11 -0500

    Actualicé repositorio

--- semana6/ejemploarray2.c
+++ semana6/ejemploarray2.c
@@ -1,13 +1,13 @@
-/*En este programa se ejemplificará el uso de los arreglos en c. Aquí se realizaron tres ejemplos y se hará otro programa con otro ejemplo*/
+/*En este programa se ejemplificará el uso de los arreglos en c. Aquí se realizó el ejemplo cuatro, para multiplicar un arreglo*/
 #include<stdio.h>
 int main() 
 {
 int i,n=10;
-float numeros[i];
+float numeros[n];
 for (i=0;i<n;i++) {
-scanf("%f",&numeros[i]);
-numeros[i]*=2; // Multiplica el entero dado por dos.
-printf("%f\n",numeros[i]); // Imprime el valor del arreglo modificado
+scanf("%f",&numeros[n]);
+numeros[n]*=2; // Multiplica el entero dado por dos.
+printf("%f\n",numeros[n]); // Imprime el valor del arreglo modificado
 } 
 return 0;
 }
