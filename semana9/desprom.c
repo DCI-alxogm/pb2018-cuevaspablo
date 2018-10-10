d330343f25d21d271cfd126cb164d946112a094b
Cuevas Cazares Jose Pablo <jpspora@gmail.com>
2018-10-10 08:58:40 -0500

    Guardé cambios.#

--- semana9/desprom.c
+++ semana9/desprom.c
@@ -1,27 +1,30 @@
 //Este programa utiliza arreglos para encontrar el promedio y la desviación estándar de un grupo de datos//
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+
 int main () 
 {
 FILE* promedio;
-FILE* resultados;
 
-int n=10,i;
-double numeros[n],sumatorio,a;
+int n=267,i;
+double numeros[n],sumatorio,a,b;
+malloc 
 promedio=fopen("promedio.txt","r");
 for (i=0;i<n;i++){
-fscanf(promedio,"%lf\n",&numeros[n]);
+fscanf(promedio,"%lf\n",&numeros[i]);
 }
 fclose(promedio);
 for (i=0;i<n;i++){
-sumatorio+=numeros[n];
+sumatorio+=numeros[i];
 }
 a=sumatorio/n;
-scanf("%lf",&a);
-resultados=fopen("resultados.txt","w");
-fprintf("%lf\n",a);
-fclose(resultados);
+for(i=0;i<n;i++){
+b=sqrt((pow((numeros[i]-a),2))/n);
+}
 
+printf("EL promedio de los datos es: %lf\n",a);
+printf("La desviación estándar de los datos es: %lf\n",b);
 
 
 return 0;
--- semana9/maximo.c
+++ semana9/maximo.c
@@ -1,22 +1,18 @@
 //Este programa utiliza apuntadores para encontrar el número máximo dentro de un conjunto de datos, los cuales son leídos a partir de un archivo//
 #include<stdio.h>
-#include<stdlib.h>
+
 int main () 
 {
 FILE* numeros;
-FILE* resultado;
 
 int n=100,i;
 double array[n];
 numeros=fopen("num_maximo.txt","r");
 for (i=0;i<n;i++)
 {
-fscanf("%lf \n",&array[n]);
+fscanf(numeros,"%lf \n",&array[i]);
 }
-fclose(numeros);
-resultado=fopen("numeros.txt","w");
-fprintf("%lf \n",array[n]);
-fclose(resultado);
+
 
 
 
