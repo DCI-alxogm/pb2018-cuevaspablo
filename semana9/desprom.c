0569e628fd8ccb336ae2522b58a3673bb4e0b7c6
Cuevas Cazares Jose Pablo <cuevascj2018@dci25-175.ugto.mx>
2018-10-15 09:08:29 -0500

    Agregué un compilable que no había podido agregar en mi casa#

--- semana9/desprom.c
+++ semana9/desprom.c
@@ -1,33 +1,37 @@
+//Este programa calcula el promedio y desviación estándar de un grupo de datos//
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 int main () 
 {
-FILE* promedio;
+
 int n,i;
-double *ptr,y,z,sumatorio=0,sumatorio2=0;
+float *ptr,y,z,sumatorio=0.000000,sumatorio2=0.000000;
+FILE* datos;
 printf("Introduce el número de elementos:");
 scanf("%i",&n);
-ptr=(double*) calloc(n,sizeof(int));
+ptr=(float*) calloc(n,sizeof(int));
 if(ptr==NULL) 
 {
 printf("¡Error! memoria no reservada,");
 exit(0);
 }
-promedio=fopen("promedio.txt","r");
+datos=fopen("promedio.txt","r");
 for (i=0;i<n;i++){
-fscanf(promedio,"%lf\n",(ptr+i));
-sumatorio+=*ptr+i;
+fscanf(datos,"%f\n",(ptr+i));
+sumatorio+=(*(ptr+i));
 }
-fclose(promedio);
+fclose(datos);
 y=(sumatorio)/n;
-promedio=fopen("promedio.txt","r");
+
+datos=fopen("promedio.txt","r");
 for (i=0;i<n;i++){
-fscanf(promedio,"%lf\n",(ptr+i));
-sumatorio2+=pow(((*(ptr+i))-y),2);
+fscanf(datos,"%f\n",(ptr+i));
+sumatorio2+=(pow(((*(ptr+i))-y),2));
 }
-fclose(promedio);
-printf("La media aritmética de los datos es:%lf\n",y);
+fclose(datos);
+z=sqrt(sumatorio2/(n-1));
+printf("La media aritmética de los datos es:%f\n",y);
 printf("La desviación estándar de los datos es: %f\n",z);
 free(ptr);
 return 0;
