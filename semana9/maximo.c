7e994aab2acf9dcced742df4e32970d4a9c1ae13
Cuevas Cazares Jose Pablo <jpspora@gmail.com>
2018-10-17 08:09:26 -0500

    Agregué un compilable que no había agregad

--- semana9/maximo.c
+++ semana9/maximo.c
@@ -1,22 +1,31 @@
 //Este programa utiliza apuntadores para encontrar el número máximo dentro de un conjunto de datos, los cuales son leídos a partir de un archivo//
 #include<stdio.h>
+#include<stdlib.h>
 
 int main () 
 {
-FILE* numeros;
 
-int n=100,i;
-double array[n];
-numeros=fopen("num_maximo.txt","r");
-for (i=0;i<n;i++)
+int n,i;
+float *ptr,nummayor;
+FILE* data;
+printf("Introduce el número de elementos:");
+scanf("%i",&n);
+ptr=(float*) calloc(n,sizeof(int));
+if(ptr==NULL) 
 {
-fscanf(numeros,"%lf \n",&array[i]);
+printf("¡Error! memoria no reservada,");
+exit(0);
 }
-for(i=0;i<n;i++) {
-if (
+data=fopen("num_maximo.txt","r");
+for (i=0;i<n;i++){
+fscanf(data,"%f\n",(ptr+i));
+if(nummayor< *(ptr+i)){
+nummayor=*(ptr+i);
+}
+}
+fclose(data);
+printf("El número más grande del conjunto de datos es:%f \n",nummayor);
 
-
-
+free(ptr);
 return 0;
-}printf("%lf \n",a;
-
+}
