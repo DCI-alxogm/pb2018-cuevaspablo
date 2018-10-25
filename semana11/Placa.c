0ab997a5db6ae690e8d17017845ab146e88e4587
Cuevas Cazares Jose Pablo <jpspora@gmail.com>
2018-10-25 15:38:01 -0500

    Avance en mi programa

--- semana11/Placa.c
+++ semana11/Placa.c
@@ -8,8 +8,7 @@ printf("El progama que estás ejecutando es: %s\n",argv[0]);
 float T1,T2,T3,T4;
 char *resultados;
 FILE *results;
-int i,j,n;
-float T[100][100];
+int i,j,n,p;
 if (arg==7){
 n=atoi(argv[1]);
 T1=atof(argv[2]);
@@ -17,12 +16,15 @@ T2=atof(argv[3]);
 T3=atof(argv[4]);
 T4=atof(argv[5]);
 resultados=argv[6];
+p=n*n;
+scanf("%i",&p);
 printf("El nombre del archivo con el que está trabajando es: %s \t  \n",argv[6]);
+float T[20][20];
 results = fopen(resultados,"w");
-n=n*n;
-fprintf(results,"T1:%f \t T2:%f \t T3:%f \t  T4:%f \t número de puntos:%i \t",T1,T2,T3,T4,n);
-for(i=0;i<n;i++) {
-for(j=0;j<n;j++) {
+
+fprintf(results,"T1:%f \t T2:%f \t T3:%f \t  T4:%f \t número de puntos:%i \t \n",T1,T2,T3,T4,n);
+for(i=0;i<p;i++) {
+for(j=0;j<p;j++) {
 T[i][j]=0;
 T[i+1][j]=0.0;
 T[i-1][j]=0.0;
@@ -30,11 +32,12 @@ T[i][j+1]=0.0;
 T[i][j-1]=0.0;
 T[i][j]=((T[i+1][j])+(T[i-1][j])+(T[i][j+1])+(T[i][j-1]))/(4);
 }
+
 T[i+1][j]=T1+T[i+1][j];
 T[i-1][j]=T2+T[i-1][j];
 T[i][j+1]=T3+T[i][j+1];
 T[i][j-1]=T4+T[i][j-1];
-fprintf(results,"%f\t %f\t  %f\t %f\t  \n",T[i+1][j],T[i-1][j],T[i][j+1],T[i][j-1]);
+fprintf(results,"%f\t %f\t  %f\t  %f\t  \n",T[i+1][j],T[i-1][j],T[i][j+1],T[i][j-1]);
 }
 fclose(results);
 }
--- semana11/matriz.c
+++ semana11/matriz.c
@@ -5,26 +5,28 @@
 int main (int arg, char*argv[]) {
 printf("El progama que estás ejecutando es: %s\n",argv[0]);
 float T1,T2,T3,T4;
-int n,i,j;
+int n=20,i,j,k,p;
 char *data;
-float T[50][50];
 FILE *resultats;
-if(arg==7) {
+if (arg==7){
 n=atoi(argv[1]);
 T1=atof(argv[2]);
 T2=atof(argv[3]);
 T3=atof(argv[4]);
 T4=atof(argv[5]);
 data=argv[6];
+p=n*n;
+scanf("%i",&p);
 printf("El nombre del archivo con el que está trabajando es: %s \t  \n",argv[6]);
+float T[20][20];
 resultats=fopen(data,"w");
-n=n*n;
-for(i=0;i<n;i++) {
- for(j=0;j<n;j++) {
-T[i][j]=(T[i][j])*(T1);
-T[i+1][j+1]=(T[i][j])*(T2);
-T[i+2][j+2]=(T[i][j])*(T3);
-T[i+3][j+3]=(T[i][j])*(T4);
+for(j=0;j<n;j++) {
+for(k=0;k<n;k++){
+T[i+k][j+k]=(T[i][j])*(T1);
+T[i+k][j+k]=(T[i][j])*(T2);
+T[i+k][j+k]=(T[i][j])*(T3);
+T[i+k][j+k]=(T[i][j])*(T4);
+}
 }
 fprintf(resultats,"T1 =%f \n",T1);
 fprintf(resultats,"T2 =%f \n",T2);
