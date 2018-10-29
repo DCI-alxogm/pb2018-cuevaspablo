a2309201bf614a1c989d872b2a7313a0bb6c166d
Cuevas Cazares Jose Pablo <cuevascj2018@dci25-176.ugto.mx>
2018-10-29 16:03:12 -0600

    Comenté mis programas

--- semana11/Placa.c
+++ semana11/Placa.c
@@ -1,4 +1,6 @@
-//Este es el segundo proyecto parcial, cuyo objetivo es encontrar la temperatura en cada punto en una placa//
+//Realizado por José Pablo Cuevas Cázares el 29/10/18, primer intento de proyecto de segundo parcial 
+
+//Este código lo hice como un extra, ya que aunque sí calculaba los valrores de la temperatura de manera correcta, no logré hacerlo funcionar con funciones, por lo que lo dejo aquí como un extra.
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
@@ -6,9 +8,10 @@
 int main (int arg, char*argv[]) {
 printf("El progama que estás ejecutando es: %s\n",argv[0]);
 float T1,T2,T3,T4,epsilon1,epsilon2;
-char *resultados1;
+char *resultadosPlaca;
 FILE *results;
 int i,j,n;
+//Las funciones atoi y atof se utilizan para cambiar una variable caracter a una variable entera o flotante.
 if (arg==8){
 n=atoi(argv[1]);
 T1=atof(argv[2]);
@@ -16,12 +19,13 @@ T2=atof(argv[3]);
 T3=atof(argv[4]);
 T4=atof(argv[5]);
 epsilon1=atof(argv[6]);
-resultados1=argv[7];
+resultadosPlaca=argv[7];
+//Se asginan los valores de los argumentos a cada variable utilizada en el programa.
 n=n+2;
 float T[n][n];
 float To[n][n];
+//Se declara el arreglo de tamaño [n][n] después de que sea declarado n, con el fin de evitar un segmentation fault.
 printf("El nombre del archivo con el que está trabajando es: %s \t  \n",argv[7]);
-printf ("%f %f %f %f \n",T1,T2,T3,T4);
 for(i=0;i<n;i++) {
 for(j=0;j<n;j++){
 T[i][j]=0;
@@ -36,25 +40,29 @@ for(j=1;j<n-1;j++){
 T[0][j]=T2;
 T[n-1][j]=T4;
 }
+//En este apartado se inicializan todos los puntos de la placa a cero, exceptuando por supuesto a los puntos de las orillas, los cuales son asignados a las temperaturas que le da el usuario, con base en las coordenadas de cada punto.
 do {
-results = fopen(resultados1,"w");
+results = fopen(resultadosPlaca,"w");
 for(j=1;j<n-1;j++){
 for(i=1;i<n-1;i++) {
 T[i][j]=(T[i+1][j]+T[i-1][j]+T[i][j+1]+T[i][j-1])/4;
 T[i][j]=(T[i][j]*1.5)+((1-1.5)*To[i][j]);
-
+//En este apartado se calcula el promedio de un punto con base en los valores de las temperaturas en sus cercanías, y también se realiza el ajuste con la ecuación utilizada en el libro.
 epsilon2=0;
 if(sqrt(pow((((T[i][j]-To[i][j])/(To[i][j]))*100),2))>epsilon2){
 epsilon2=sqrt(pow((((T[i][j]-To[i][j])/(To[i][j]))*100),2));
 }
 To[i][j]=T[i][j];
+//En este apartado se calcula epsilon2, y se establece una condición para saber si es mayor que el epsilon dado por el usuario, lo cual se dejará de cumplir hasta que epsilon2 sea mayor que epsilon1.
 fprintf(results,"%f\t",T[i][j]);
 }
+//Se imprimen los valores de cada punto.
 fprintf(results,"\n");
 }
 
 fclose(results);
 }
+//Se cierra el archivo. 
 while(epsilon2>epsilon1);
 }
 
--- semana11/resultados.c
+++ semana11/resultados.c
@@ -1,25 +1,26 @@
+//Realizado por José Pablo Cuevas Cázares el 29/10/18, función la cual sirve para calcular los resutlados del proyecto de segundo parcial.
 #include <stdio.h>
 #include<math.h>
 #include<stdlib.h>
-//Esta función opera los resultados que son necesitados en la función main
-void resultados(float epsilon1) //En esta parte se le da como argumento a la función resultados el epsilon leído a partir de un archivo
+//Esta función opera los resultados que son necesitados en la función main.
+void resultados(float epsilon1) //En esta parte se le da como argumento a la función resultados el epsilon leído a partir de un archivo.
 {
 int z=0;
-float epsilon2;
+float epsilon2,epsilon3;
 char prenom[20];
 FILE *results;
 FILE *inicial;
 FILE *data;
 int i,j,n;
-//Aquí se declaran todas las variables, así como los archivos, uno para leer n, otro para abrir el archivo inicial del cual se toman los valores de la matriz para las orillas y otro más llamado data, para imprimir los resultados del archivo
+//Aquí se declaran todas las variables, así como los archivo//Realizado por José Pablo Cuevas Cázares el 29/10/18, primer intento de proyecto de segundo parcials, uno para leer n, otro para abrir el archivo inicial del cual se toman los valores de la matriz para las orillas y otro más llamado data, para imprimir los resultados del archivo.
 data=fopen("datos.txt","r");
 fscanf(data,"%i",&n);
 fclose(data);
 n=n+2;
-//Escaneo y asginación de n
+//Escaneo y asginación de n.
 float T[n][n];
 float To[n][n];
-//Declaración de los arreglos de tamaño [n][n]
+//Declaración de los arreglos de tamaño [n][n].
 inicial = fopen("inicial.txt","r");
 
 for(j=0; j<n; j++)
@@ -30,39 +31,41 @@ fscanf(inicial,"%f \n",&T[i][j]);
 }
 }
 fclose(inicial);
-//En este for se escanean todos los datos impresos en el archivo, acción que se realizó en la función main
+//En este for se escanean todos los datos impresos en el archivo, acción que se realizó en la función main.
 do {
 for(j=1;j<n-1;j++){
 for(i=1;i<n-1;i++) {
 T[i][j]=(T[i+1][j]+T[i-1][j]+T[i][j+1]+T[i][j-1])/4;
-//Se calcula el promedio de un punto en la placa
+//Se calcula el promedio de un punto en la placa.
 T[i][j]=(T[i][j]*1.5)+((1-1.5)*To[i][j]);
-//Tomando la ecuación del libro, aquí se hace un ajuste de la temperatura tomada
+//Tomando la ecuación del libro, aquí se hace un ajuste de la temperatura tomada.
 epsilon2=0;
-//Se inicializa epsilon2 en cero, para que pueda ir tomando valores hasta que no se cumpla la condición establecida por el while
+//Se inicializa epsilon2 en cero, para que pueda ir tomando valores hasta que no se cumpla la condición establecida por el while.
 if(sqrt(pow((((T[i][j]-To[i][j])/(To[i][j]))*100),2))>epsilon2){
 epsilon2=sqrt(pow((((T[i][j]-To[i][j])/(To[i][j]))*100),2));
 }
 To[i][j]=T[i][j];
 }
-//En este apartado se le da un valor a epsilon
+//En este apartado se le da un valor a epsilon.
 }
-//En este for se realizan todas las operaciones de la temperatura, tomando los valores que fueron escaneados en el archivo inicial
+//En este for se realizan todas las operaciones de la temperatura, tomando los valores que fueron escaneados en el archivo inicial.
 sprintf(prenom,"%iresultados.txt",z);
-results= fopen(prenom,"w");
-if(z%45==0){
+results = fopen(prenom,"w");
+if(epsilon1>epsilon1 && z%45==0){
 for(j=1;j<n-1;j++){
 for(i=1;i<n-1;i++) {
 fprintf(results,"%f\t",T[i][j]);
 }
 fprintf(results,"\n");
 }
-z=z+1;
 }
 fclose(results);
+z=z+1;
 }
-//En esta sección es cuando se imprimen los resultados en muchos archivos
-while(epsilon2>epsilon1);//Lo que está dentro del while se realizará hasta que el epsilon nuevo sea mayor que el que fue dado por el usuario
+
+//En esta sección es cuando se imprimen los resultados en muchos archivos.
+while(epsilon2>epsilon1 && z<70);
+printf("%f\n",z);//Lo que está dentro del while se realizará hasta que el epsilon nuevo sea mayor que el que fue dado por el usuario.
 }
 
 
